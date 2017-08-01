394408e394bc4aff6fb7408ab56e2586cf85ae4b
zeljko94 <zeljko-10000@hotmail.com>
2017-08-01 15:39:09 +0200

    Modeli

---
+++ EvidencijaRadnogVremena/Models/FreeDays.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EvidencijaRadnogVremena.Models
+{
+    public class FreeDays
+    {
+        public long FreeDaysId { get; set; }
+        public int NumberOfFreeDays { get; set; }
+        public int NumberOfFreeDaysLeft { get; set; }
+        public int Year { get; set; }
+        public long EmployeeId { get; set; }
+    }
+}
---
+++ EvidencijaRadnogVremena/Models/WorkingDays.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EvidencijaRadnogVremena.Models
+{
+    public class WorkingDays
+    {
+        public long WorkingDaysId { get; set; }
+        public bool Monday { get; set; }
+        public bool Tuesday { get; set; }
+        public bool Wednesday { get; set; }
+        public bool Thursday { get; set; }
+        public bool Friday { get; set; }
+        public bool Saturday { get; set; }
+        public bool Sunday { get; set; }
+        public long CompanyId { get; set; }
+    }
+}
